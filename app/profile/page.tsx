d63095f3f49399704a871f1f978cd6cdb09ab62d
Андрей Слива <cryptomaniaaccton@gmail.com>
2025-08-04 14:31:20 +0200

    popitka 10000

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -11,7 +11,7 @@ const ProfilePage = () => {
         <div className='flex flex-col items-center justify-center h-screen'>
             <div className='w-[400px] items-center flex flex-col h-[400px] border border-black]'>
                 <p className='text-[20px]'>Username: {dbUser?.username}</p>
-                <p className='text-[20px]'>Balance: {dbUser?.balance}</p>
+                <p className='text-[20px]'>Your balance: {dbUser?.balance}</p>
                 <button className='p-2 border border-black'>Вывести средства</button>
             </div>
         </div>
