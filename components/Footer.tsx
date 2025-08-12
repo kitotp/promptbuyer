14650cdb97d32df6a04410339353edc7fca6a1fb
Andrey Sliva <andreysliva@Andreys-MacBook-Air-2.local>
2025-08-12 21:23:52 +0200

    h changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='fixed bottom-0 left-0 w-full h-[75px] rounded-t-[24px] bg-amber-700'>
+        <div className='fixed bottom-0 left-0 w-full h-[var(--footer-height)] rounded-t-[24px] bg-amber-700'>
             <div className='flex gap-3 flex-row h-full items-center justify-center'>
                 <Link href={'/tasks'}>Tasks</Link>
                 <Link href={'/profile'}>Profile</Link>
